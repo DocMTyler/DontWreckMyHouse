2a2c098852362c3879adabc91c50cafd7935ef35
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-04-05 21:09:16 -0500

    start on reservation service

--- DontWreckMyHouse.BLL/ReservationService.cs
+++ DontWreckMyHouse.BLL/ReservationService.cs
@@ -1,8 +1,41 @@
-﻿using System;
+﻿using DontWreckMyHouse.Core.Interfaces;
+using DontWreckMyHouse.Core.Models;
+using System;
+using System.Collections.Generic;
 
 namespace DontWreckMyHouse.BLL
 {
     public class ReservationService
     {
+        private readonly IReservationRepository reserveRepo;
+        private readonly IGuestRepository guestRepo;
+        private readonly IHostLocationRepository hostRepo;
+
+        public ReservationService(IReservationRepository reserveRepo, IGuestRepository guestRepo, IHostLocationRepository hostRepo)
+        {
+            this.reserveRepo = reserveRepo;
+            this.guestRepo = guestRepo;
+            this.hostRepo = hostRepo;
+        }
+
+        public List<Reservation> FindReservationsByHost(HostLocation host)
+        {
+            
+            
+            return ValidateHost(host) ? reserveRepo.GetReservationsByHost(host) : new List<Reservation>();
+        }
+
+        public bool ValidateHost(HostLocation host)
+        {
+            var hostList = hostRepo.GetAll();
+            foreach(var repoHost in hostList)
+            {
+                if(host.ID == repoHost.ID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
