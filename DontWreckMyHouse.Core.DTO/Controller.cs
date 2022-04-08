2d753fb8b1560b38bb40c1b131cfc34b655020e0
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-04-07 21:47:34 -0500

    DAL, BLL, Program, Ninject, Tests all working. Menu built. Controller and View skeleton.

--- DontWreckMyHouse.Core.DTO/Controller.cs
+++ DontWreckMyHouse.Core.DTO/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DontWreckMyHouse.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,81 @@ namespace DontWreckMyHouse.UI
 {
     public class Controller
     {
+        private readonly ReservationService reservationService;
+        private readonly GuestService guestService;
+        private readonly HostService hostService;
+        private readonly View view;
+
+        public Controller(ReservationService reservationService, GuestService guestService, HostService hostService, View view)
+        {
+            this.reservationService = reservationService;
+            this.guestService = guestService;
+            this.hostService = hostService;
+            this.view = view;
+        }
+
         public void Run()
         {
+            view.DisplayHeader("Do NOT Wreck My House!!!");
+            try
+            {
+                RunMenu();
+            }catch(Exception e)
+            {
+                view.DisplayException(e);
+            }
+            view.DisplayHeader("Aufwiedersehen...");
+        }
 
+        private void RunMenu()
+        {
+            bool running = true;
+            string selection;
+            do
+            {
+                selection = view.GetMenuChoice();
+                switch (selection)
+                {
+                    case "0":
+                        running = false;
+                        break;
+                    case "1":
+                        ViewReservationsForHost();
+                        break;
+                    case "2":
+                        MakeAReservation();
+                        break;
+                    case "3":
+                        EditAReservation();
+                        break;
+                    case "4":
+                        CancelAReservation();
+                        break;
+                    default:
+                        Console.WriteLine("Input not recognized. Please enter 0-4.");
+                        break;
+                }
+            } while (running);
+        }
+
+        private void CancelAReservation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditAReservation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MakeAReservation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewReservationsForHost()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- DontWreckMyHouse.Core.DTO/View.cs
+++ DontWreckMyHouse.Core.DTO/View.cs
@@ -8,5 +8,37 @@ namespace DontWreckMyHouse.UI
 {
     public class View
     {
+        private readonly ConsoleIO io;
+
+        public View(ConsoleIO io)
+        {
+            this.io = io;
+        }
+        
+        public void DisplayHeader(string message)
+        {
+            io.PrintLine("");
+            io.PrintLine(message);
+            io.PrintLine(new string('=', message.Length));
+        }
+
+        public void DisplayException(Exception ex)
+        {
+            DisplayHeader("A critical error occurred:");
+            io.PrintLine(ex.Message);
+        }
+
+        public string GetMenuChoice()
+        {
+            io.PrintLine("Main Menu");
+            io.PrintLine("=========");
+            io.PrintLine("0. Exit");
+            io.PrintLine("1. View Reservations for Host");
+            io.PrintLine("2. Make a Reservation");
+            io.PrintLine("3. Edit a Reservation");
+            io.PrintLine("4. Cancel a Reservation");
+            io.Print("Select [0-4]:");
+            return Console.ReadLine();
+        }
     }
 }
